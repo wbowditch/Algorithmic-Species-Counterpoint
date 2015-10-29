c6afce46fdcf4af19c5daa8208220e865dbb6a77
William Bowditch <bowditcw@bc.edu>
2015-10-29 15:30:17 -0400

    initialization
    Reads in the base line and key, determines if valid.

---
+++ Initialization.py
@@ -0,0 +1,20 @@
+#
+#
+#
+#
+
+bLine=[]
+k=""
+
+def inputBassLine(bassLine,key):
+    if (goodBassLine(bassLine,key)):
+        bLine=bassLine
+        k=key
+    else:
+        print "Bad bass line, try again"
+
+def goodBassLine(bassLine,key):
+    
+    
+    
+    
